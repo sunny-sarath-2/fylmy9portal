0be33a2e717717e7ae036825d00e4637f8f5ddf1
sunny-sarath-2 <sunnsarath2@gmail.com>
2019-10-02 00:11:30 +0530

    jwt changes

--- routes/api/review.route.js
+++ routes/api/review.route.js
@@ -6,7 +6,7 @@ const review_controller = require("../../controllers/review.controller");
 router.get("/getall", review_controller.review_get_all);
 router.get("/", review_controller.review_get_published);
 router.get("/:_id", review_controller.review_get_one);
-router.post("", common.verify_token, review_controller.review_post);
+router.post("", review_controller.review_post); //, common.verify_token
 router.put("", common.verify_token, review_controller.review_put);
 router.delete("/:_id", common.verify_token, review_controller.review_delete);
 module.exports = router;
